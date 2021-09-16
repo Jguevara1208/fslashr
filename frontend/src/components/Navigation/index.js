8c5138cd744e227528ee70988433d2e719e9b2b7
Jordan <jordansacct@gmail.com>
2021-09-16 16:15:49 -0700

    finished nav bar styling'

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -3,20 +3,22 @@ import { useSelector } from "react-redux";
 import { AiOutlineCloudUpload } from 'react-icons/ai';
 import NavLogo from './navbar-logo'
 import ProfileButton from "./ProfileButton";
-import './Navigation.css';
+import nav from './Navigation.module.css';
 
 function Navigation( {isLoaded} ) {
     const sessionUser = useSelector(state => state.session.user);
     
     return (
-        <nav>
-            <NavLink exact to='/home'>{NavLogo}</NavLink>
-            {isLoaded && (
-                <>
-                    <NavLink to='/images/upload'><AiOutlineCloudUpload style={{fontSize: '2.2em'}} /></NavLink>
-                    <ProfileButton user={sessionUser} />
-                </>
-            )}
+        <nav className={nav.main}>
+            <div className={nav.container}>
+                <NavLink exact to='/home'>{NavLogo}</NavLink>
+                {isLoaded && (
+                    <div className={nav.buttons}>
+                        <NavLink className={nav.upload} to='/images/upload'><AiOutlineCloudUpload style={{fontSize: '2.2em'}} /></NavLink>
+                        <ProfileButton user={sessionUser} />
+                    </div>
+                )}
+            </div>
         </nav>
     );
 }
