6433f9988b8bff825b160c345dfbbd3d2ad8c9ca
Jordan <jordansacct@gmail.com>
2021-11-10 14:23:16 -0800

    fixed photos

--- frontend/src/components/SplashPage/splashPhotos.js
+++ frontend/src/components/SplashPage/splashPhotos.js
@@ -1,14 +1,12 @@
 const photos = [
-    'https://github.com/Jguevara1208/soloProjectPhotos/blob/main/860e8b248da74dc549809a9f7865e8a8.jpeg?raw=true',
-    'https://github.com/Jguevara1208/soloProjectPhotos/blob/main/024fe94afcaf9a566645c091ba664a14.jpeg?raw=true',
-    'https://github.com/Jguevara1208/soloProjectPhotos/blob/main/Sophie12-24-6.jpg?raw=true',
-    'https://github.com/Jguevara1208/soloProjectPhotos/blob/main/aecd359f8a78786ee12a818eca541c1f.jpeg?raw=true',
-    'https://github.com/Jguevara1208/soloProjectPhotos/blob/main/eb25cbe29792e2979414f2b980e5bd03.jpeg?raw=true',
-    'https://github.com/Jguevara1208/soloProjectPhotos/blob/main/b3592e0e6d9237daa46f503aac8eff3f.jpeg?raw=true',
-    'https://github.com/Jguevara1208/soloProjectPhotos/blob/main/541b316ace7964610d915d46440dc8ae.jpeg?raw=true',
-    'https://github.com/Jguevara1208/soloProjectPhotos/blob/main/Sophie12-24-9.jpg?raw=true',
-    'https://github.com/Jguevara1208/soloProjectPhotos/blob/main/Sophie12-24-1.jpg?raw=true',
-    'https://github.com/Jguevara1208/soloProjectPhotos/blob/main/DSC02000.jpg?raw=true',
+    'https://cdn.discordapp.com/attachments/908117032069918770/908117109081522197/860e8b248da74dc549809a9f7865e8a8.jpeg',
+    'https://cdn.discordapp.com/attachments/908117032069918770/908117101703757884/aecd359f8a78786ee12a818eca541c1f.jpeg',
+    'https://cdn.discordapp.com/attachments/908117032069918770/908117087296290846/024fe94afcaf9a566645c091ba664a14.jpeg',
+    'https://cdn.discordapp.com/attachments/908117032069918770/908117090316193812/b3592e0e6d9237daa46f503aac8eff3f.jpeg',
+    'https://cdn.discordapp.com/attachments/908117032069918770/908117083332689970/eb25cbe29792e2979414f2b980e5bd03.jpeg',
+    'https://cdn.discordapp.com/attachments/908117032069918770/908117119219154954/DSC02044.jpeg',
+    'https://cdn.discordapp.com/attachments/908117032069918770/908117115804983326/Sophie12-24-6.jpeg',
+    'https://cdn.discordapp.com/attachments/908117032069918770/908117136562614302/DSC02000.jpeg',
 ]
 
 export default photos
